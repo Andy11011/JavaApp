c470e7be67df87014e0f28af1a13c1cfce35f957
Andrei Pavlov <andrei_pavlov3@epam.com>
2025-07-22 18:37:34 +0500

    Gradle Java framework hands-on project

---
+++ src/main/java/com/example/region_demo/RegionController.java
@@ -0,0 +1,21 @@
+package com.example.regionapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.amazonaws.util.EC2MetadataUtils;
+
+@RestController
+public class RegionController {
+
+    @GetMapping("/region")
+    public String getRegionAndAZ() {
+        String az = null, region = null;
+        try {
+            az = EC2MetadataUtils.getAvailabilityZone();
+            region = EC2MetadataUtils.getEC2InstanceRegion();
+        } catch (Exception e) {
+            return "Unable to retrieve EC2 metadata: " + e.getMessage();
+        }
+        return String.format("Region: %s, Availability Zone: %s", region, az);
+    }
+}
---
+++ src/test/java/com/example/region_demo/RegionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.region_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
