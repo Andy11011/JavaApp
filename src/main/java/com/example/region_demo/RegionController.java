316c0f01f74315e99cd1536419d54df51c3f94ff
Andrei Pavlov <andrei_pavlov3@epam.com>
2025-07-22 18:48:32 +0500

    Fixed error with the package name

--- src/main/java/com/example/region_demo/RegionController.java
+++ src/main/java/com/example/region_demo/RegionController.java
@@ -1,4 +1,4 @@
-package com.example.regionapp;
+package com.example.region_demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
